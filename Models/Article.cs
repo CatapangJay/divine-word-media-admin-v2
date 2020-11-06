a6d584c3e66bb0026e5dd8c8dceb97704b6dcf02
CatapangJay <38466188+CatapangJay@users.noreply.github.com>
2020-11-07 04:38:03 +0800

    Add project files.

---
+++ Models/Article.cs
@@ -0,0 +1,30 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivineWordAdmin.Models
+{
+    [FirestoreData]
+    public class Article
+    {
+        [FirestoreProperty(Name = "articleurl")]
+        public string ArticleUrl { get; set; }
+
+        [FirestoreProperty(Name = "author")]
+        public string Author { get; set; }
+
+        [FirestoreProperty(Name = "body")]
+        public string Body { get; set; }
+
+        [FirestoreProperty(Name = "header")]
+        public string Header { get; set; }
+
+        [FirestoreProperty(Name = "imageurl")]
+        public string ImageUrl { get; set; }
+
+        [FirestoreProperty(Name = "publishdate")]
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ Services/ArticleService.cs
@@ -0,0 +1,46 @@
+﻿using DivineWordAdmin.Enums;
+using DivineWordAdmin.Models;
+using Google.Cloud.Firestore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivineWordAdmin.Services
+{
+    public class ArticleService
+    {
+        private FirestoreDb _firestoreDb;
+        private readonly string collectionName = Collections.articles.ToString();
+        public ArticleService()
+        {
+            _firestoreDb = InitializeFirestore();
+        }
+
+        public async Task<IEnumerable<Article>> GetAllArticles()
+        {
+            Query query = _firestoreDb.Collection(collectionName);
+            QuerySnapshot articlesSnapshots = await query.GetSnapshotAsync();
+
+            IEnumerable<Article> articles = articlesSnapshots.Where(snap => snap.Exists).Select(snap => { return snap.ConvertTo<Article>(); });
+
+            return articles;
+        }
+        private FirestoreDb InitializeFirestore()
+        {
+            string fbConfigPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            string config = string.Empty;
+
+            using (StreamReader reader = new StreamReader(fbConfigPath))
+                config = reader.ReadToEnd();
+
+            JObject configObj = JObject.Parse(config);
+
+            string fbProjectId = configObj.SelectToken("project_id").ToString();
+
+            return FirestoreDb.Create(fbProjectId);
+        }
+    }
+}
